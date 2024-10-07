f9be135ad15a351b44920072c4e97cf1a4443403
Vadhir <lomuner@gmail.com>
2024-10-07 05:42:17 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,58 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard(); // Genera una carta cuando carga la página por primera vez
+
+  function generateRandomCard() {
+    const simbolos = ["hearts", "spades", "clubs", "diamonds"];
+    const valores = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    const cartaPrincipal = document.getElementById("carta");
+    const simboloTop = document.getElementById("top-left");
+    const simboloBottom = document.getElementById("bottom-right");
+    const numeroDeCarta = document.getElementById("card-number");
+
+    const simbolosRandom = Math.floor(Math.random() * simbolos.length);
+    const numeroRandom = Math.floor(Math.random() * valores.length);
+
+    const simbolo = simbolos[simbolosRandom];
+    const valor = valores[numeroRandom];
+
+    cartaPrincipal.classList.remove("hearts", "spades", "clubs", "diamonds");
+
+    cartaPrincipal.classList.add(simbolo);
+
+    numeroDeCarta.textContent = valor;
+    simboloTop.textContent = getSimboloDeMazo(simbolo);
+    simboloBottom.textContent = getSimboloDeMazo(simbolo);
+  }
+
+  function getSimboloDeMazo(simbolo) {
+    switch (simbolo) {
+      case "hearts":
+        return "♥";
+      case "spades":
+        return "♠";
+      case "clubs":
+        return "♣";
+      case "diamonds":
+        return "♦";
+      default:
+        return "";
+    }
+  }
 };
